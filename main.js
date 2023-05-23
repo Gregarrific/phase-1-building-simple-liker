716dbe93b092ef35e625e8610f78824ac81b970f
Gregory Dutton <greg.dutton@gmail.com>
2023-05-22 20:48:40 -0700

    lab complete

--- main.js
+++ main.js
@@ -3,9 +3,40 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const hidden = document.querySelector('div#modal');
+hidden.classList.toggle('hidden');
+document.addEventListener('click', like);
 
+function like(event) {
+  let location = event.target;
+  if (location.className === 'like-glyph') {
+    mimicServerCall()
+    .then(response => {
+      location.classList.replace('like-glyph', 'activated-heart');
+      location.innerHTML = FULL_HEART;
+    })
+    .catch(error => {
+      hidden.querySelector('p#modal-message').innerHTML = error;
+      hidden.classList.replace('hidden', 'visible');
+      setTimeout(showError, 3000);
+    });
+  } else if (location.className === 'activated-heart') {
+    mimicServerCall()
+    .then(response => {
+      location.classList.replace('activated-heart', 'like-glyph');
+      location.innerHTML = EMPTY_HEART;
+    })
+    .catch(error => {
+      hidden.querySelector('p#modal-message').innerHTML = error;
+      hidden.classList.replace('hidden', 'visible');
+      setTimeout(showError, 3000);
+    });
+  }
+}
 
-
+function showError() {
+  return hidden.classList.replace('visible', 'hidden');
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
